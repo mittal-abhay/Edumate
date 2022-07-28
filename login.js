fc8cc13b8aa922475f10a5b9728c07891811645e
subham <kumarsubham75861@gmail.com>
2022-07-28 15:17:47 +0530

    login & registration done

--- login.js
+++ login.js
@@ -26,9 +26,9 @@ const container = document.querySelector(".container"),
     })
 
     // js code to appear signup and login form
-    signUp.addEventListener("click", ( )=>{
-        container.classList.add("active");
-    });
-    login.addEventListener("click", ( )=>{
-        container.classList.remove("active");
-    });
+    // signUp.addEventListener("click", ( )=>{
+    //     container.classList.add("active");
+    // });
+    // login.addEventListener("click", ( )=>{
+    //     container.classList.remove("active");
+    // });
